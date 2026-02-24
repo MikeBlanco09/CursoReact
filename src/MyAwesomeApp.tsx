21230cd2273c40f5c9576d58f61bf0e8170a01c3
Michael Blanco <michael.blanco0901@gmail.com>
2026-02-24 15:08:40 -0600

    First commit

---
+++ src/MyAwesomeApp.tsx
@@ -0,0 +1,14 @@
+
+export function MyAwesomeApp() {
+
+    const firstName = 'Michael';
+    const lastName = 'Blanco';
+
+
+    return (
+        <>
+            <h1> {firstName} </h1>
+            <h3> {lastName} </h3>
+        </>
+    )
+}
