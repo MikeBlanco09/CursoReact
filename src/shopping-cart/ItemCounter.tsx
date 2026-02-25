0d8f2a1d89f73d3d822468080b880eef85d72aff
Michael Blanco <michael.blanco0901@gmail.com>
2026-02-25 16:00:49 -0600

    Clases 43,44,45 : UseStates

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props {
     name: string;
     quantity?: number;
 }
 
-export const ItemCounter = ({ name, quantity }: Props) => {
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
+
+    const [count, setCount] = useState(quantity);
+
+    const handleAdd = () => {
+        setCount(count + 1)
+    }
+
+    const handleSubstract = () => {
+        if (count === 1) return;
+
+        setCount(count - 1)
+    }
+
     return (
         <section style={{
             display: 'flex',
@@ -18,9 +31,9 @@ export const ItemCounter = ({ name, quantity }: Props) => {
             }}>
                 {name}
             </span>
-            <button>+1</button>
-            <span>{quantity}</span>
-            <button>-1</button>
+            <button onClick={handleAdd}>+1</button>
+            <span>{count}</span>
+            <button onClick={handleSubstract}>-1</button>
         </section>
     )
 }
