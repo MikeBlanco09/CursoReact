67f22ce41918a3587fd5b63a74409621cf314b1f
Michael Blanco <michael.blanco0901@gmail.com>
2026-02-25 11:18:36 -0600

    Clases 39 y 40

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,14 +1,38 @@
+import type { CSSProperties } from "react";
+
+const firstName = 'Michael';
+const lastName = 'Blanco';
+
+const favoriteGames = ['Elden Ring', 'Smash', 'Metal Gear'];
+const isActive = false;
+
+const address = {
+    zipCode: 'ABC-123',
+    country: 'Costa Rica',
+};
+
+const myStyles: CSSProperties = {
+    backgroundColor: '#fafafa',
+    borderRadius: 20,
+    padding: 10,
+    marginTop: 30,
+}
 
 export function MyAwesomeApp() {
-
-    const firstName = 'Michael';
-    const lastName = 'Blanco';
-
 
     return (
         <>
             <h1> {firstName} </h1>
             <h3> {lastName} </h3>
+
+            <p>{favoriteGames.join(", ")}</p>
+
+            <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+
+            <p style={myStyles} >
+                {JSON.stringify(address)}
+            </p>
+
         </>
     )
 }
